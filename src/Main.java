09001b322aa9d5dcf7a337f45ed5ac8fa6ad11ac
Liza go <elizaveta2000333@gmail.com>
2023-06-09 23:20:39 +0300

    code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // переменная для начального счёта
+
         int check = 100;
         int amount = 1100;
 
@@ -12,14 +12,9 @@ public class Main {
         }
         int total = check + amount + bonus;
 
-        System.out.println("Итоговый счет: "+ total);
+        System.out.println("Итоговый счет: " + total);
         System.out.println("Количество бонусов:" + bonus);
 
-        // суммы пополнения и тп
 
-        // Условным оператором проверяете, превысила ли
-        // сумма пополнения порог, и для этих двух разных
-        // сценариев рассчитываете сумму бонуса и выводите
-        // на экран.
     }
 }
